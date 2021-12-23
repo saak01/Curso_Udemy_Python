842cc4012ab8c9f794f76eface73d58e3c68f56f
Sak <joaovictoralvescosta051@gmail.com>
2021-12-23 20:44:22 -0300

    Mais Conteudo 23-12-21

---
+++ Aula-42_Zip.py
@@ -0,0 +1,22 @@
+"""
+Zip 
+
+zip() -> Cria um iteravel (Zip Objetc) que agrega elementos de cada dos iteraveis passados como entrada em pares 
+
+#Exemplos:
+
+lista1 = [1,2,3]
+lista2 = [4,5,6]
+#Com Lista
+print("Lista")
+print(list(zip(lista1,lista2)))
+#Com Tupla
+print("Tupla")
+print(tuple(zip(lista1,lista2)))
+#Com set
+print("Set")
+print(set(zip(lista1,lista2)))
+
+"""
+
+ 
