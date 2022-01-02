32f165f513824a1720af69457e39e920295237ff
Sak <joaovictoralvescosta051@gmail.com>
2022-01-02 17:51:26 -0300

    Mais Conteudo 02/01/22

--- Aula-42_Zip.py
+++ Aula-42_Zip.py
@@ -17,6 +17,29 @@ print(tuple(zip(lista1,lista2)))
 print("Set")
 print(set(zip(lista1,lista2)))
 
-"""
+#O zip utiliza como tamanho o menor paramentro como iterável. Isso significa que, quando estiver trabalhando com 
+iteraveis de valores diferentes, o  "break" sera no menor.
+lista1=[1,2,3,4]
+lista2=[55,3,4]
+lista3=[44,3,4,3,321]
+zips = zip(lista1,lista2,lista3)
+print(list(zips))
+ #Pode-se usar varios tipos de iteraveis com o zip.
+        #Revisao: Para descompactar um conjunto de iteraveis é usado o *, deste modo segue o exemplo com o zip:
 
- 
+"""
+dados = [(0,1,2), (3,4,5),(6,7,8)]
+print(dados)
+print(*dados)
+
+#Exemplo mais complexos:
+
+prova1 = [80,91,78]
+prova2 = [98,89,53]
+alunos = ['Pedro','Maria','Carlos']
+final = {dado[0]: max(dado[1], dado[2]) for dado in zip(alunos,prova1,prova2)}
+print(final)
+ #Podemos utilizar map():
+
+final= zip(alunos,map(lambda nota: max(nota,zip(prova1,prova2))))
+print(dict(final))
---
+++ Aula-43_Erros_Mais_Comuns_em_Python.py
@@ -0,0 +1,28 @@
+"""
+Erros Mais comuns em python:
+
+-SyntaxError:
+Ocorre quando existe um erro de sintaxe, exemplo: ao inves de print, foi executado "printf" este comando não é reconhecido pelo python
+assim ocasionando um SyntaxError.
+
+-Name error:
+Ocorre quando uma variavel ou função não foi definida. Exemplo: 
+
+print(geek)
+
+Exemplo 2:
+
+geek() ->uma função não existente.
+
+-TypeError:
+Ocorre quando uma função/operação/ação  é aplicada a um tipo  errado. Exemplo:
+
+print(len(4))
+
+-IndexError:
+Ocorre quando tentamos acessar um elemento em uma lista ou outro tipo de dado indexado utilzando index errado. Exemplo
+nome = "joao"
+print(nome[9])
+"""
+
+
