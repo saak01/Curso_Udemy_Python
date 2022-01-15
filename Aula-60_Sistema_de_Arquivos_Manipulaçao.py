1df83f7cb5c7f5edd87ae844062baa45facba5bf
Sak <joaovictoralvescosta051@gmail.com>
2022-01-15 17:14:01 -0300

    Mais Conteudo 15/01/22

---
+++ Aula-60_Sistema_de_Arquivos_Manipulaçao.py
@@ -0,0 +1,24 @@
+"""
+Sistema de Arquivos - Manipulação
+-----------------------------------------------------------------------------------------------------------------------------------------
+#Descobrindo se arquivo/Dir existe.
+
+import os 
+
+# Paths Relativos
+print(os.path.exists("50_testefunçoes.py"))#True
+print(os.path.exists("texto.txt"))#False
+
+#Path absolutos
+
+print(os.path.exists(/home/))#True
+
+----------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+"""
+
+
